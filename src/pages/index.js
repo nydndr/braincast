2d4ca4af1b51f3ab9b71ac0468a83159ae21c946
Nicoly Dandara Costa Oliveira Santos <dandararchive@gmail.com>
2023-02-07 17:09:30 -0300

    bye world

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
+import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,15 +17,60 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer">
-            <h2 className={inter.className}>Ei, Sara!</h2>
-            <p className={inter.className}>Nosso QR Code funciona, ebaaa!</p>
-          </a>
+        <div className={styles.hero}>
+          <header className={styles.header}>
+            <img src={"./blogo.png"} alt="Braincast Logo"></img>
+          </header>
+
+          <div>
+            <h1 className={styles.title}> 20 Anos </h1>
+            <h2 className={styles.subtitle}> Um evento hoje no futuro.</h2>
+
+            <div className={styles.videodiv}>
+              <img src={"./play.png"}></img>
+            </div>
+
+            <p className={styles.texto}>Carlos Merigo, Host do Braincast</p>
+
+            <div className={styles.arrow}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="0.8"
+                stroke="currentColor">
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                />
+              </svg>
+            </div>
+          </div>
+
+          <div className={styles.experience}>
+            <div>
+              <p className={styles.instrucoes}>
+                Coloque seu telefone na horizontal e insira no seu óculos VR
+              </p>
+              <img src={"./phone.png"}></img>
+              <p className={styles.instrucoes}>
+                {" "}
+                A experiência já vai começar!
+              </p>
+              <a className={styles.comecar} href="#experience">
+                {" "}
+                Entrar
+              </a>
+            </div>
+          </div>
+
+          <div id="experience" className={styles.vr}>
+            <div>
+              <button>Programação</button>
+              <button>Ir a Palestra</button>
+            </div>
+          </div>
         </div>
       </main>
     </>
